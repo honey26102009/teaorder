f6143f77d15d07afb878e733e119f7a0d888961e
honey26102009 <honey26102009@gmail.com>
2022-09-25 20:52:42 +0700

    first commit

--- teaorder/wsgi.py
+++ teaorder/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for teaselling project.
+WSGI config for teaorder project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teaselling.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teaorder.settings')
 
 application = get_wsgi_application()
